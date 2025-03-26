76ac49458f55f047c07425d431d352e8e21d15f2
Marco-Rocco <marcorocco28@icloud.com>
2025-03-26 16:32:05 +0100

    add: ersponse to server ('server del mio blog')

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 1;
+const port = 1000;
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
 
 app.listen(port, () => {
     console.log('server attivo su port ' + port);
